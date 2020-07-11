4034db90012a28832737a707c19dd0773ad52cf8
xavigh <fgargallo@gmail.com>
2020-07-11 16:20:37 +0200

    upgrading

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,5 @@
+
+import '@angular/localize/init';
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
